9d09d42315a4822fcac84d14ddc4e09ba8094899
Rownak Mazumder <rmazumder1033@gmail.com>
2023-12-03 18:19:05 +0530

    Endpoints Changed

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 //its a api
 ///
 const api = axios.create({
-  baseURL: "https://web-production-a472.up.railway.app/api",
+  baseURL: "https://server.sociolinq.com/api",
   headers: { "x-api-key": "a8518942-17ea-44a6-b4e1-a974189a9a90" },
   withCredentials: true,
 });
